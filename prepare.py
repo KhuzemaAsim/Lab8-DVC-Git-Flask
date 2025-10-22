c01fe83333f58bc3e2f47b40e2c326dbe052bbe5
KhuzemaAsim <khuzemaasim123@gmail.com>
2025-10-22 18:44:11 +0500

    Add DVC pipeline for Iris dataset

---
+++ prepare.py
@@ -0,0 +1,8 @@
+import os
+os.makedirs("data", exist_ok=True)
+
+from sklearn.datasets import load_iris
+import pandas as pd
+data = load_iris(as_frame=True)
+df = pd.concat([data.data, pd.Series(data.target, name='target')], axis=1)
+df.to_csv('data/iris.csv', index=False)
